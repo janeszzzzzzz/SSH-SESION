fd4ae4a1b8c55bfec21f17f43b3112f3341d95f7
JANETH ESQUIVEL PAEZ - ing.PEDRIN <janethesquiveljeep@gmail.com>
2025-09-22 08:28:54 -0600

    feat:suma

--- app.py
+++ app.py
@@ -9,3 +9,7 @@ print(f"IP ADDRESS:{ipaddr}")
 
 for i in range(10):
     print(f"count:{i}")
+
+num_a=int(input("DAME EL PRIMER NUMERO: "))
+num_b=int(input("DAME EL SEGUNDO NUMERO: "))
+print(f"La suma es: {num_a + num_b}")
